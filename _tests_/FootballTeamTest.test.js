62abfcfd0aec81e5e4bd90a80ccbc9c4dba5bd3d
jpickard393 <jpickard@and.digital>
2022-05-15 16:24:02 +0100

    all failing tests fixed and unwanted tests removed

--- _tests_/FootballTeamTest.test.js
+++ _tests_/FootballTeamTest.test.js
@@ -5,11 +5,6 @@ const threeGamesWon = 3;
 describe ('Football Team' ,()=>{
     it('Should return 3',()=>{
         const footballTeam = new FootballTeam(threeGamesWon);
-        expect(footballTeam.getGamesWon).toBe(threeGamesWon);
+        expect(footballTeam.getGamesWon()).toBe(threeGamesWon);
     });
-
-    it.each([[0],[1],[3],[10]])('should set gamesWon in constructor',(gamesWon)=>{
-        const footballTeam = new FootballTeam(gamesWon);
-        expect(footballTeam.getGamesWon()).toBe(gamesWon);
-    })
 });
